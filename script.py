245c3584ec24bc90c543a046f3439c3f0adac517
Andres Gonzalez <afgquinones@gmail.com>
2026-08-21 10:52:49 -0500

    feat: add row and column count function

--- script.py
+++ script.py
@@ -3,19 +3,22 @@ import pandas as pd
 df = pd.read_csv('train.csv')
 
 def newTopic(title, command_action):
-    print("\n" + "="*50)
-    print(title)
-    print("="*50)
-    
-    if callable(command_action):
-        result = command_action()
-        if result is not None:
-            print(result)
-    else:
-        print(command_action)
+  print('\n' + '=' * 50)
+  print(title)
+  print('=' * 50)
+  if callable(command_action):
+    result = command_action()
+    if result is not None:
+      print(result)
+  else:
+    print(command_action)
 
-newTopic("FIRST ROWs:", lambda: df.head())
-newTopic("LAST ROWS:", lambda: df.tail())
-newTopic("COLUMN NAMES:", df.columns.tolist())
-newTopic("GENERAL INFORMATION (DATA TYPES AND MEMORY USE ):", lambda: df.info())
-newTopic("NULLs COUNT:", lambda: df.isnull().sum())
+
+newTopic('FIRST ROWS:', lambda: df.head())
+newTopic('LAST ROWS:', lambda: df.tail())
+newTopic('COLUMN NAMES:', df.columns.tolist())
+newTopic('GENERAL INFORMATION (DATA TYPES AND MEMORY USE):', lambda: df.info())
+newTopic('NULLs COUNT:', lambda: df.isnull().sum())
+
+rows, column = df.shape
+print(f"TOTAL SIZE: {rows}, ROWS AND {column} COLUMNS")
