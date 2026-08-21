348a8eaa56ca27c6ccd2e61e1d1ae9c95586d629
Andres Gonzalez <afgquinones@gmail.com>
2026-08-21 10:04:20 -0500

    View the basic information of the dataframee

--- script.py
+++ script.py
@@ -2,4 +2,18 @@ import pandas as pd
 
 df = pd.read_csv('train.csv')
 
+print("="*50)
+print("FIRST ROWs:")
+print("="*50)
 print(df.head())
+
+print("\n" + "="*50)
+print("LAST ROWS:")
+print("="*50)
+print(df.tail())
+
+print("\n" + "="*50)
+print("COLUMN NAMES:")
+print("="*50)
+print(df.columns.tolist())
+
