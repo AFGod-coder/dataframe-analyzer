0d6aa7c85fd713fa841df6605a3b35fff6b89c39
Andres Gonzalez <afgquinones@gmail.com>
2026-08-27 13:30:55 -0500

    refactor: add try-catch for read main failes

--- script.py
+++ script.py
@@ -3,19 +3,22 @@ import pandas as pd
 from utils import clean_dataframe, plot_sales_histogram
 
 ARCHIVO_ORIGINAL = "train.csv"
+ARCHIVO_LIMPIO = "clean_train.csv"
 
 if __name__ == "__main__":
     if not os.path.exists(ARCHIVO_ORIGINAL):
         print(f"Error: Archivo '{ARCHIVO_ORIGINAL}' no encontrado.")
         exit(1)
 
-    df_sucio = pd.read_csv(ARCHIVO_ORIGINAL)
-    df_limpio = clean_dataframe(df_sucio)
-
-    plot_sales_histogram(
-        df=df_limpio,
-        title="Distribucion de unidades vendidas",
-        xlabel="Numero de unidades vendidas",
-        ylabel="Frecuencia (dias)",
-        bins=50
-    )
+    try:
+        df_sucio = pd.read_csv(ARCHIVO_ORIGINAL)
+        
+        
+        
+    except FileNotFoundError:
+        print("El archivo no se encuentra en el directorio")
+        
+    try:
+        df_limpio = pd.read_csv(ARCHIVO_LIMPIO)
+    except FileNotFoundError:
+        df_limpio = clean_dataframe(df_sucio)
